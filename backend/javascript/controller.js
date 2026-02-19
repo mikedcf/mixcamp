6ffddbd310fd7c8fd68f70497a694058cd238486
mikedcf <mikedcf74@gmail.com>
2026-02-19 08:51:50 -0300

    ajustando o codigo do email ipv4 mudando porta

--- backend/javascript/controller.js
+++ backend/javascript/controller.js
@@ -19,16 +19,17 @@ const { validarEmail, validarSenha, validarCaracteres } = require('./auth');
 
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-  // força IPv4
-  family: 4,
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+    family: 4,
 });
+  
 
 
 
